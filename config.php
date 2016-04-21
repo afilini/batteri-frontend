f4d49229715ddcc66f31ba2160a6c329c01e0dba
Alekos Filini <alekos.filini@gmail.com>
2016-04-21 09:10:01 +0200

    Aggiornato config

--- config.php
+++ config.php
@@ -12,6 +12,6 @@ $_CONFIG['db_user'] = 'gba';
 $_CONFIG['db_password'] = 'password';
 $_CONFIG['db_port'] = 3306;
 
-$_CONFIG['root'] = '/bcms';
+$_CONFIG['root'] = '/';
 
 $_CONFIG['magic'] = '06da66efdb0c95f723d2b06369505eadbca9ff7f721cec1f7437a7fd8cb9c947'; // sha256('battericontest');
